1594ea985d44300283e1abf4513ece8b4495394e
OMSV <os3349@gmail.com>
2012-08-10 23:33:36 -0430

    Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0b2b978985e85f2273d8bc721ffcd36da198436095cd191d0dd9f8de9f93f9b81f5e5931d8e49926a157a928ec1b4211b5de1aa94e62c9f26e000ea6d8ac095e'
